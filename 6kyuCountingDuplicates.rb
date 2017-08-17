58fc876a528b3601cf40172d608c8299805bb335
Richard Mands <richard.mands@sage.com>
2017-08-17 15:13:46 +0100

    update

---
+++ 6kyuCountingDuplicates.rb
@@ -0,0 +1,15 @@
+
+def duplicate_count(text)
+  arr = text.downcase.split('')
+  dupe_arr = []
+  index = 0
+  arr.each do |char|
+    arr[index] = nil
+    dupe_arr << char.downcase unless dupe_arr.include?(char) || !arr.include?(char)
+    index += 1
+  end
+  puts dupe_arr.length
+  dupe_arr.length
+end
+
+duplicate_count('aabbBcdefg')
